abba7c6c234a68eda078136311085048c0767904
jonasvanhoof <jonasvanhoof@inuits.eu>
2021-08-30 15:52:37 +0200

    WIP FilterComponent

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -3,14 +3,16 @@
     v-if="filters.length > 0"
     class="flex flex-wrap flex-row w-full"
   >
+    <p>{{ setAllTag.value }}</p>
     <FilterTag
+      v-model:isSelected="setAllTag"
       :filter="filterAll"
-      :selected="true"
       @click="toggleFilters"
     />
     <FilterTag
       v-for="tag in filters"
       :key="tag"
+      v-model:isSelected="tags"
       :filter="tag"
       @click="addToSelectedTags(tag)"
     />
@@ -18,7 +20,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType, SetupContext } from 'vue'
+import { defineComponent, PropType, SetupContext, ref, watch } from 'vue'
 import { FilterTag } from 'coghent-vue-3-component-library'
 
 export default defineComponent({
@@ -44,22 +46,38 @@ export default defineComponent({
   emits: ['selected'],
   setup(props: any, { emit }: SetupContext) {
     let selectedTags: Array<string> = []
+    let tags = ref(false)
+    let setAllTag = ref(true)
 
     const toggleFilters = () => {
-      console.log("TOGGLE FILTERS");
-      selectedTags = [];
+      setAllTag.value = !setAllTag.value
+      selectedTags = []
       emit('selected', selectedTags)
     }
 
     const addToSelectedTags = (tag: string) => {
-      console.log(`Add "${tag}" to filters`);
+      console.log(`Add "${tag}" to filters`)
       if (selectedTags.includes(tag, 0)) {
         selectedTags.splice(selectedTags.indexOf(tag), 1)
-      } else selectedTags.push(tag)
+        if (selectedTags.length <= 0) {
+          tags.value = false
+          setAllTag.value = true
+        }
+      } else {
+        selectedTags.push(tag)
+        tags.value = true
+        setAllTag.value = false
+      }
       emit('selected', selectedTags)
     }
+    watch(tags, (value: boolean) => {
+      console.log('TAGS watch', value)
+    })
+    watch(setAllTag, (value: boolean) => {
+      console.log('SETALLTAG watch', value)
+    })
 
-    return { toggleFilters, addToSelectedTags }
+    return { toggleFilters, addToSelectedTags, tags, setAllTag }
   },
 })
 </script>
