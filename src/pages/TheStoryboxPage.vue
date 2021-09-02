480e715d32f1f77c1839783de145c814e742668d
Kevin Davis <scazi@inuits.eu>
2021-09-02 17:19:13 +0200

    Created carousel

--- src/pages/TheStoryboxPage.vue
+++ src/pages/TheStoryboxPage.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="">
+<div class="flex flex-col gap-16">
     <card-component :large="false">
         <p>Testing the small component</p>
     </card-component>
@@ -7,15 +7,18 @@
     <card-component :large="true">
         <p>Testing the large component</p>
     </card-component>
+    <the-carousel/>
     </div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { CardComponent } from 'coghent-vue-3-component-library'
+import TheCarousel from '../components/TheCarousel.vue'
 
 export default defineComponent({
     components:{
-        CardComponent
+        CardComponent,
+        TheCarousel
     },
     setup() {
         
