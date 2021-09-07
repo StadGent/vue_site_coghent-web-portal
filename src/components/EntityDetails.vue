fed92bc86d6e132a5101cdc1d376e206f91a9c35
Jeroen Wouters <jeroenwouters@inuits.eu>
2021-09-07 10:24:27 +0200

    add carousel to single

--- src/components/EntityDetails.vue
+++ src/components/EntityDetails.vue
@@ -1,88 +1,20 @@
 <template>
-  <div
-    v-if="result"
-    class="grid grid-cols-2 mt-20"
-  >
-    <section
-      v-if="result.Entity?.mediafiles"
-      class="flex items-center justify-between px-10"
-    >
-      <svg
-        v-show="result.Entity?.mediafiles.length > 1"
-        class="cursor-pointer transform rotate-180 flex-shrink-0 mr-2"
-        width="34"
-        height="24"
-        viewBox="0 0 17 12"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        @click="prevImage"
-      >
-        <path
-          d="M1 6H16"
-          stroke="#B65099"
-          stroke-miterlimit="10"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        />
-        <path
-          d="M11 1L16 6L11 11"
-          stroke="#B65099"
-          stroke-miterlimit="10"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        />
-      </svg>
-      <img
-        class="w-9/12"
-        :src="result.Entity?.mediafiles[selectedImageIndex].original_file_location"
-      >
-      <svg
-        v-show="result.Entity?.mediafiles.length > 1"
-        class="cursor-pointer flex-shrink-0 ml-2"
-        width="34"
-        height="24"
-        viewBox="0 0 17 12"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        @click="nextImage"
-      >
-        <path
-          d="M1 6H16"
-          stroke="#B65099"
-          stroke-miterlimit="10"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        />
-        <path
-          d="M11 1L16 6L11 11"
-          stroke="#B65099"
-          stroke-miterlimit="10"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        />
-      </svg>
+  <div v-if="result" class="grid grid-cols-2 mt-20">
+    <section class="flex items-center justify-between px-10">
+      <the-carousel v-if="photos" :source="photos" />
     </section>
     <CardComponent :large='true' v-if="result">
       <div class="flex flex-col bg-background-medium px-10 py-10">
         <h1 class="text-lg font-bold">
           {{ result.Entity?.title[0]?.value }}
         </h1>
-        <div
-          v-for="metaData in result.Entity?.metadata"
-          :key="metaData.value"
-          class="pt-5 font-light"
-        >
+        <div v-for="metaData in result.Entity?.metadata" :key="metaData.value" class="pt-5 font-light">
           <p v-show="metaData.key === 'description'">
             {{ metaData.value }}
           </p>
         </div>
         <div class="pt-5 font-medium">
-          <span
-            v-for="metaData in result.Entity?.metadata"
-            v-show="metaData.key === 'type'"
-            :key="metaData.value"
-            class="inline-block px-2 py-2 bg-background-dark mr-4 bg-opacity-50"
-          >{{
+          <span v-for="metaData in result.Entity?.metadata" v-show="metaData.key === 'type'" :key="metaData.value" class="inline-block px-2 py-2 bg-background-dark mr-4 bg-opacity-50">{{
             metaData.value
           }}</span>
         </div>
@@ -90,7 +22,7 @@
     </CardComponent>
     <section class="col-span-2">
       <h2 class="font-bold text-2xl w-full text-center pt-10">
-        {{ t("details.discover") }}
+        {{ t('details.discover') }}
       </h2>
       <div class="pt-5 font-medium pb-2">
         <relation-tag
@@ -100,12 +32,7 @@
           @click="setRelation(relation.key)"
         />
       </div>
-      <the-masonry
-        v-if="resultRelation"
-        :small="true"
-        :entities="resultRelation.Entities"
-        :loading="loadingRelation"
-      />
+      <the-masonry v-if="resultRelation" :small="true" :entities="resultRelation.Entities" :loading="loadingRelation" />
     </section>
   </div>
 </template>
@@ -114,7 +41,7 @@
 import { defineComponent, ref } from 'vue'
 import { useRoute } from 'vue-router'
 import { useQuery } from '@vue/apollo-composable'
-import { GetEntityByIdDocument, GetFullEntitiesDocument, CardComponent } from 'coghent-vue-3-component-library'
+import { GetEntityByIdDocument, GetFullEntitiesDocument, TheCarousel, CardComponent } from 'coghent-vue-3-component-library'
 import RelationTag from './RelationTag.vue'
 import TheMasonry from './TheMasonry.vue'
 import { useI18n } from 'vue-i18n'
@@ -123,12 +50,13 @@ const asString = (x: string | string[]) => (Array.isArray(x) ? x[0] : x)
 
 export default defineComponent({
   name: 'EntityDetails',
-  components: { RelationTag, TheMasonry, CardComponent },
+  components: { RelationTag, TheMasonry, CardComponent, TheCarousel },
   setup: () => {
     const id = asString(useRoute().params['entityID'])
     const { result, onResult } = useQuery(GetEntityByIdDocument, { id })
     const selectedImageIndex = ref<number>(0)
     const relations = ref([])
+    const photos = ref<string[] | undefined>()
 
     const nextImage = () => {
       if (result.value && result.value.Entity && result.value.Entity.mediafiles) {
@@ -142,13 +70,12 @@ export default defineComponent({
       }
     }
 
-
     const {
       result: resultRelation,
       loading: loadingRelation,
       variables,
     } = useQuery<any>(GetFullEntitiesDocument, {
-      searchQuery: 'niets',
+      searchValue: { value: 'niets' },
     })
 
     const setRelation = (id: string) => {
@@ -173,9 +100,20 @@ export default defineComponent({
     }
     }
 
-    const { t } = useI18n();
+    onResult((queryResult: any) => {
+      const photosArray: string[] = []
+      queryResult.data.Entity?.mediafiles.forEach((value: any) => {
+        console.log(value)
+        if (value.original_file_location) {
+          photosArray.push(value.original_file_location)
+        }
+      })
+      photos.value = photosArray
+    })
 
-    return { result, selectedImageIndex, nextImage, prevImage, relations, resultRelation, setRelation, loadingRelation, t }
+    const { t } = useI18n()
+
+    return { result, selectedImageIndex, nextImage, prevImage, relations, resultRelation, setRelation, loadingRelation, t, photos }
   },
 })
 </script>
