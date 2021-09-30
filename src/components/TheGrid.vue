e34825222c6f2b127e9a723d0ee8b95616aa661d
Scazi <scazi@inuits.eu>
2021-09-30 16:31:11 +0200

    CTA work in progress

--- src/components/TheGrid.vue
+++ src/components/TheGrid.vue
@@ -4,47 +4,16 @@
       {{ t('main.title') }} <span class="text-accent-purple">{{ t('main.rich') }}</span>
     </h1>
     <div class="w-8/12 py-6">
-      <base-search
-        v-model="keyword"
-        :label="t('main.library')"
-        @on-click="getData"
-      />
+      <base-search v-model="keyword" :label="t('main.library')" @on-click="getData" />
     </div>
-    <Filter
-      :filter-all="'Alle werken'"
-      :filters="['Foto collectie', 'Associatie 1', 'Associatie 2', 'Associatie 3']"
-      @selected="setSelectedFilters"
-    />
-    <div
-      v-show="loading"
-      class="pt-10"
-    >
-      <svg
-        class="animate-spin -ml-1 mr-3 h-20 w-20 text-white"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-      >
-        <circle
-          class="opacity-25"
-          cx="12"
-          cy="12"
-          r="10"
-          stroke="currentColor"
-          stroke-width="4"
-        />
-        <path
-          class="opacity-75"
-          fill="currentColor"
-          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-        />
+    <Filter :filter-all="'Alle werken'" :filters="['Foto collectie', 'Associatie 1', 'Associatie 2', 'Associatie 3']" @selected="setSelectedFilters" />
+    <div v-show="loading" class="pt-10">
+      <svg class="animate-spin -ml-1 mr-3 h-20 w-20 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4" />
+        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z" />
       </svg>
     </div>
-    <the-masonry
-      v-if="result"
-      :entities="result.Entities"
-      :loading="loading"
-    />
+    <the-masonry v-if="result" :entities="result.Entities" :loading="loading" />
   </section>
 </template>
 
@@ -65,19 +34,30 @@ export default defineComponent({
   setup: () => {
     let listWithCTA: Object[] = []
     const keyword = ref<string>('')
-    
-    const { result, loading, fetchMore } = useQuery(GetFullEntitiesDocument, {
-        limit: 20,
-        skip: 0,
-        searchValue: {
-          value: keyword.value,
-          isAsc: false,
-          key: "title",
-        },
-      });
 
-    const { result: Relations, fetchMore: fetchMoreRelations, loading: relationsLoading } = useQuery<any>(GetRelationsDocument, {
-      searchValue: {value: keyword.value},
+    const { result, loading, fetchMore, onResult } = useQuery(GetFullEntitiesDocument, {
+      limit: 20,
+      skip: 0,
+      searchValue: {
+        value: keyword.value,
+        isAsc: false,
+        key: 'title',
+      },
+    })
+
+    const {
+      result: Relations,
+      fetchMore: fetchMoreRelations,
+      loading: relationsLoading,
+    } = useQuery<any>(GetRelationsDocument, {
+      searchValue: { value: keyword.value },
+    })
+
+    onResult((queryResult: any) => {
+      // queryResult.forEach((element: any) => {
+      //   if (element.index % 5 == 0) listWithCTA.push('CTA')
+      //   listWithCTA.push(element)
+      // })
     })
 
     const getData = () => {
@@ -102,6 +82,7 @@ export default defineComponent({
       result,
       t,
       setSelectedFilters,
+      listWithCTA
     }
   },
 })
