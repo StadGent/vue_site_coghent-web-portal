ab2a43aec3791844dd4e6a97d5feff2143e19b42
Scazi <scazi@inuits.eu>
2021-09-30 21:44:05 +0200

    WIP CTA

--- src/components/TheGrid.vue
+++ src/components/TheGrid.vue
@@ -1,16 +1,48 @@
 <template>
   <section class="flex flex-col items-center">
-    <h1 class="text-5xl font-bold w-8/12 text-center py-10 block leading-normal">
-      {{ t('main.title') }} <span class="text-accent-purple">{{ t('main.rich') }}</span>
+    <h1
+      class="text-5xl font-bold w-8/12 text-center py-10 block leading-normal"
+    >
+      {{ t("main.title") }}
+      <span class="text-accent-purple">{{ t("main.rich") }}</span>
     </h1>
     <div class="w-8/12 py-6">
-      <base-search v-model="keyword" :label="t('main.library')" @on-click="getData" />
+      <base-search
+        v-model="keyword"
+        :label="t('main.library')"
+        @on-click="getData"
+      />
     </div>
-    <Filter :filter-all="'Alle werken'" :filters="['Foto collectie', 'Associatie 1', 'Associatie 2', 'Associatie 3']" @selected="setSelectedFilters" />
+    <Filter
+      :filter-all="'Alle werken'"
+      :filters="[
+        'Foto collectie',
+        'Associatie 1',
+        'Associatie 2',
+        'Associatie 3',
+      ]"
+      @selected="setSelectedFilters"
+    />
     <div v-show="loading" class="pt-10">
-      <svg class="animate-spin -ml-1 mr-3 h-20 w-20 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4" />
-        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z" />
+      <svg
+        class="animate-spin -ml-1 mr-3 h-20 w-20 text-white"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <circle
+          class="opacity-25"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="currentColor"
+          stroke-width="4"
+        />
+        <path
+          class="opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+        />
       </svg>
     </div>
     <the-masonry v-if="result" :entities="result.Entities" :loading="loading" />
@@ -18,32 +50,38 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, ref, watch } from 'vue'
-import { useQuery } from '@vue/apollo-composable'
-import { GetFullEntitiesDocument, BaseSearch, GetRelationsDocument } from 'coghent-vue-3-component-library'
-import 'coghent-vue-3-component-library/lib/index.css'
-import TheMasonry from './TheMasonry.vue'
-import { useI18n } from 'vue-i18n'
-import Filter from './Filter.vue'
-import { CTA } from '../call-to-action'
+import { defineComponent, onMounted, ref, watch } from "vue";
+import { useQuery } from "@vue/apollo-composable";
+import {
+  GetFullEntitiesDocument,
+  BaseSearch,
+  GetRelationsDocument,
+} from "coghent-vue-3-component-library";
+import "coghent-vue-3-component-library/lib/index.css";
+import TheMasonry from "./TheMasonry.vue";
+import { useI18n } from "vue-i18n";
+import Filter from "./Filter.vue";
 
 export default defineComponent({
-  name: 'AssetGrid',
+  name: "AssetGrid",
   components: { BaseSearch, TheMasonry, Filter },
   props: {},
   setup: () => {
-    let listWithCTA: Object[] = []
-    const keyword = ref<string>('')
+    let listWithCTA: Object[] = [];
+    const keyword = ref<string>("");
 
-    const { result, loading, fetchMore, onResult } = useQuery(GetFullEntitiesDocument, {
-      limit: 20,
-      skip: 0,
-      searchValue: {
-        value: keyword.value,
-        isAsc: false,
-        key: 'title',
-      },
-    })
+    const { result, loading, fetchMore, onResult } = useQuery(
+      GetFullEntitiesDocument,
+      {
+        limit: 20,
+        skip: 0,
+        searchValue: {
+          value: keyword.value,
+          isAsc: false,
+          key: "title",
+        },
+      }
+    );
 
     const {
       result: Relations,
@@ -51,14 +89,19 @@ export default defineComponent({
       loading: relationsLoading,
     } = useQuery<any>(GetRelationsDocument, {
       searchValue: { value: keyword.value },
-    })
+    });
 
     onResult((queryResult: any) => {
-      // queryResult.forEach((element: any) => {
-      //   if (element.index % 5 == 0) listWithCTA.push('CTA')
-      //   listWithCTA.push(element)
-      // })
-    })
+      console.log("queryresult", queryResult);
+      let counter = 0
+      queryResult.data.Entities.results.forEach((entity: Object) => {
+        counter++
+        if(counter % 6 === 0) listWithCTA.push('CTA')
+        listWithCTA.push(entity)
+        
+      });
+      console.log('listwithCTA', listWithCTA)
+    });
 
     const getData = () => {
       fetchMore({
@@ -66,14 +109,14 @@ export default defineComponent({
           searchValue: { value: keyword.value },
         },
         updateQuery: (prev, { fetchMoreResult: res }) => res || prev,
-      })
-    }
+      });
+    };
 
     const setSelectedFilters = (values: string[]) => {
-      console.log(values)
-    }
+      console.log(values);
+    };
 
-    const { t } = useI18n()
+    const { t } = useI18n();
 
     return {
       keyword,
@@ -82,8 +125,8 @@ export default defineComponent({
       result,
       t,
       setSelectedFilters,
-      listWithCTA
-    }
+      listWithCTA,
+    };
   },
-})
+});
 </script>
