a54755f87387a5163943151bade6b827081f1a22
Scazi <scazi@inuits.eu>
2021-10-18 15:27:39 +0200

    Changed env variables to be in line with other projects

--- src/environment.ts
+++ src/environment.ts
@@ -17,7 +17,6 @@ type domain = {
     oauthBaseUrl: string;
     search_api: string;
     collection_api: string;
-    storage_api: string;
     graphQl: string;
     lazyLoad: string;
     userEditUrl: string;
@@ -34,9 +33,8 @@ type domain = {
       process.env.APOLLO_CLIENT_SECRET || "445cac0c-101f-4916-8cb2-dade093d38a7",
     oauthBaseUrl:
       process.env.OAUTH_BASE_URL || "http://localhost:8080/auth/realms/dams/",
-    collection_api: process.env.COLLECTION_API || "http://localhost:8000",
-    search_api: process.env.SEARCH_API || "http://localhost:8002",
-    storage_api: process.env.STORAGE_API || "http://localhost:8001",
+    collection_api: process.env.COLLECTION_API_URL || "http://localhost:8000",
+    search_api: process.env.SEARCH_API_URL || "http://localhost:8002",
     graphQl: process.env.GRAPHQL || "http://localhost:8070/api/graphql",
     lazyLoad: process.env.LAZYLOAD || "http://localhost:8070/lazy-loading.svg",
     userEditUrl: process.env.USER_EDIT_URL || "http://localhost:8080/auth/admin/master/console/#/realms/dams/users/"
