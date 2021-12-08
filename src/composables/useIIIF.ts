05f541aa2471d74f5b7624b262de8671f4488172
Jeroen Wouters <jeroenwouters@inuits.eu>
2021-12-08 12:40:39 +0100

    Get images from storage api instead of iiif temp

--- src/composables/useIIIF.ts
+++ src/composables/useIIIF.ts
@@ -4,11 +4,13 @@ import StoreFactory from '@/stores/StoreFactory'
 const useIIIF = () => {
   const configStore = StoreFactory.get(ConfigStore)
   const iiifUrl = configStore.config.value.iiifLink
+  const storageLink = configStore.config.value.storageLink
 
   const noImageUrl = '/no-image.png'
 
   const generateUrl = (filename: string, format: 'square' | 'full', size: number = 1000) => {
-    return `${iiifUrl}iiif/3/${filename}/${format}/,${size}/0/default.jpg`
+    // return `${iiifUrl}iiif/3/${filename}/${format}/${size},/0/default.jpg`
+    return `${storageLink}download/${filename}`
   }
 
   return {
