f718434d8b353796d9f1749ea74378df8b6b8a60
Jeroen Wouters <jeroenwouters@inuits.eu>
2021-12-08 11:07:30 +0100

    revert prev change until update api

--- src/components/TheGrid.vue
+++ src/components/TheGrid.vue
@@ -86,6 +86,7 @@ export default defineComponent({
         searchValue: {
           value: searchQueryForQuery.value,
           isAsc: false,
+          key: 'title',
           relation_filter: getSelectedFilters.value,
           randomization: true,
           seed: randomValue.value.toString(),
@@ -108,6 +109,7 @@ export default defineComponent({
         searchValue: {
           value: searchQueryForQuery.value,
           isAsc: false,
+          key: 'title',
           relation_filter: props.defaultRelations.length > 0 ? props.defaultRelations : [],
           randomization: true,
           seed: randomValue.value.toString(),
