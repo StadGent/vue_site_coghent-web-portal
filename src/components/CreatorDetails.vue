ae10b5592800640b1f87bcd1fd8463d983358b36
Berend <berend@inuits.eu>
2022-01-06 09:26:08 +0100

    Refs #85604 grid shows only works from same creator

--- src/components/CreatorDetails.vue
+++ src/components/CreatorDetails.vue
@@ -21,7 +21,7 @@
   </div>
   <div class="flex flex-col w-full px-3">
   <h2 class="md:text-5xl sm:text-4xl text-3xl font-bold w-full text-center py-10 block leading-normal">{{t('creator.moreWorks') + ' ' + person.fullName}}</h2>
-<the-grid :small="true" :no-header="true" class="sm:mt-24"/>
+<the-grid :small="true" :no-header="true" class="sm:mt-24" :defaultRelations="relationStringArray"/>
   </div>
 </div>
 </template>
@@ -32,7 +32,7 @@ import TheGrid from './TheGrid.vue'
 import { useI18n } from 'vue-i18n'
 import { useRoute, useRouter } from 'vue-router'
 import { useQuery } from '@vue/apollo-composable'
-import { GetCreatorByIdDocument, BaseButton, BaseMetaData, BaseMetaDataTab } from 'coghent-vue-3-component-library'
+import { GetCreatorByIdDocument, BaseButton, BaseMetaData, BaseMetaDataTab, FullRelationFragment } from 'coghent-vue-3-component-library'
 
 const asString = (x: string | string[]) => (Array.isArray(x) ? x[0] : x)
 
@@ -63,6 +63,8 @@ export default defineComponent({
     const route = useRoute()
     const metadata = ref<Metadata[]>()
     const person = ref<Person>()
+        const relationStringArray = ref<string[]>([])
+    const relationsLabelArray = ref<string[]>([])
     const tabs: Array<string> = ['Design Museum Gent', 'STAM', 'Industriemuseum']
 
     const goToPreviousPage = () => {
@@ -85,7 +87,13 @@ export default defineComponent({
           fullName: getNameFromMetadata('fullName'),
           extraInfo:metadata.value
         }
-        console.log(person.value)
+        
+        queryResult.data.Entity?.relations
+          .forEach((relation: any) => {
+            relationStringArray.value.push(relation.key)
+            console.log(relation.key)
+            relation.label && relationsLabelArray.value.push(relation.label)
+          })
         
       }
       else{
@@ -98,7 +106,8 @@ export default defineComponent({
       goToPreviousPage,
       route,
       person,
-      result
+      result,
+      relationStringArray
     }
   },
 })
