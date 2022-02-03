5ef9eb4db042597553b3da179063b2f064f6d237
Berend <berend@inuits.eu>
2022-02-03 16:01:49 +0100

    Cookies for disclaimer modal & button in top right corner to open big disclaimer modal

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <div id="modals" />
-  <div class="bg-background-light font-body py-6 min-h-screen flex flex-col items-center">
+  <div class="bg-background-light font-body py-6 min-h-screen flex flex-col items-center overflow-x-hidden">
     <div class="container">
       <the-header />
       <router-view />
       <details-modal :scroll="false" />
       <creative-modal />
+      <disclaimer-button/>
       <disclaimer-pop-up />
     </div>
   </div>
@@ -21,10 +22,11 @@ import StoreFactory from './stores/StoreFactory'
 import CreativeModal from './components/CreativeModal.vue'
 import DetailsModal from './components/DetailsModal.vue'
 import DisclaimerPopUp from './components/DisclaimerPopUp.vue'
+import DisclaimerButton from './components/DisclaimerButton.vue'
 
 export default defineComponent({
   name: 'App',
-  components: { TheHeader, CreativeModal, DetailsModal, DisclaimerPopUp },
+  components: { TheHeader, CreativeModal, DetailsModal, DisclaimerPopUp, DisclaimerButton },
   setup: () => {
     const { t } = useI18n()
     const configStore = StoreFactory.get(ConfigStore)
---
+++ src/components/DisclaimerButton.vue
@@ -0,0 +1,37 @@
+<template>
+<div width="80" height="77" class="absolute top-0 right-0 overflow-hidden text-accent-yellow">
+            <svg width="120" height="119" viewBox="0 0 120 119" class="relative -top-10 -right-10" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path
+                opacity="1"
+                d="M6.38128 49.1539C3.20326 32.893 13.809 17.1346 30.0699 13.9566L70.3846 6.07751C86.6455 2.89948 102.404 13.5052 105.582 29.7661L113.461 70.0808C116.639 86.3417 106.033 102.1 89.7724 105.278L49.4577 113.157C33.1968 116.335 17.4384 105.729 14.2604 89.4686L6.38128 49.1539Z"
+                fill="currentColor"
+              />
+            </svg>
+            <base-button
+            class="relative -top-28 -right-20"
+            custom-style="round-white"
+          custom-icon="bigInfo"
+          :icon-shown="true"
+          :on-click="openDisclaimerModal"
+        />
+          </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import {BaseButton} from 'coghent-vue-3-component-library'
+import {useDisclaimerModal} from './DisclaimerPopUp.vue'
+
+
+export default defineComponent({
+  name: 'DisclaimerButton',
+  components: {BaseButton},
+  setup: () => {
+      const {openDisclaimerModal} = useDisclaimerModal()
+
+        return {openDisclaimerModal}
+  },
+})
+</script>
+
+<style></style>
