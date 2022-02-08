dc90351e2b3c109d08758a7c321f1d862efb27f6
Jeroen Wouters <jeroenwouters@inuits.eu>
2022-02-08 10:59:41 +0100

    Add menu item #89210

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex justify-between flex-wrap sm:flex-nowrap">
+  <div class="flex justify-between flex-wrap sm:flex-nowrap mt-12 md:mt-0">
     <div class="flex gap-8">
       <div class="flex flex-col items-center">
         <router-link to="/home" class="flex items-center sm:ml-0 ml-3">
@@ -21,22 +21,21 @@
               </filter>
             </defs>
           </svg>
-          <div class="flex flex-col items-center mt-5">
-            <h2 class="ml-4 text-lg" :class="{ [`font-bold`]: isHomeActive }">
+          <div class="flex flex-col items-center mt-5 ml-2">
+            <h2 class="md:text-lg text-center" :class="{ [`font-bold`]: isHomeActive }">
               {{ t('header.discover') }}
             </h2>
             <div class="w-2 h-2 mt-2" :class="{ [`bg-accent-yellow rounded`]: isHomeActive }" />
           </div>
         </router-link>
       </div>
-      <!-- <div class="flex flex-col items-center pt-3">
-        <router-link to="/pavilion" class="flex items-center invisible">
-          <h2 class="text-lg -mt-3 md:-mt-0 lg:mt-0" :class="{ [`font-bold`]: isPavilionActive }">
-            {{ t('header.pavilion') }}
+      <div class="flex flex-col items-center pt-8 md:pt-5">
+        <a target="_blank" href="https://www.collectie.gent/over-het-project" class="flex items-center">
+          <h2 class="md:text-lg -mt-3 md:-mt-0 lg:mt-0 text-center">
+            {{ t('header.about') }}
           </h2>
-        </router-link>
-        <div class="w-2 h-2 mt-3" :class="{ [`bg-accent-yellow rounded -ml-5 sm:ml-0 mb-3 sm:mb-0`]: isPavilionActive }" />
-      </div> -->
+        </a>
+      </div>
     </div>
 
     <!-- <div class="flex ml-3 invisible">
