d685712182b97254fd406354ccf4fcfede785197
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2022-02-17 10:23:32 +0100

    mediafile in and text are centered in the section with a padding

--- src/components/DetailsModal.vue
+++ src/components/DetailsModal.vue
@@ -15,34 +15,36 @@
   <BaseModal :large="true" :scroll="true" :modal-state="DetailsModalState.state" @hide-modal="closeDetailsModal" customStyles="z-40">
     <section v-if="entity" class="bg-background-medium flex flex-col h-full overflow-y-auto pb-12 sm:pb-0">
       <section class="flex flex-col lg:flex-row h-10/12 sm:h-5/6">
-        <section class="bg-background-light h-auto lg:w-1/3 px-4">
-          <h1 class="text-2xl font-black my-2 text-center lg:text-left lg:ml-6 mt-6">
-            {{ entity.title[0]?.value }}
-          </h1>
-          <div class="m-3 lg:ml-6 lg:mt-6">
-            <base-meta-data :key-word="t('details.modal.objectNumber')" :type="entity.objectNumber[0]?.value" :error-text="t('details.modal.unknown')" />
-            <base-meta-data :key-word="t('details.modal.objectName')" :type="getObjectName(entity.metadataCollection)" :error-text="t('details.modal.unknown')" />
-            <base-meta-data
-              v-if="collectieNaam && collectieNaam.nestedMetaData && collectieNaam.nestedMetaData.title.length != 0"
-              :key-word="t('details.modal.collectieNaam')"
-              :type="collectieNaam.nestedMetaData.title[0].value"
-              :error-text="t('details.modal.unknown')"
-              :clickable="true"
-              @click="goToRelation(collectieNaam.nestedMetaData.id)"
-            />
-            <base-meta-data
-              v-if="getName(entity, 'MaterieelDing.beheerder').name != 'onbekend'"
-              :key-word="t('details.modal.instellingNaam')"
-              :type="getName(entity, 'MaterieelDing.beheerder').name"
-              :error-text="t('details.modal.unknown')"
-              :clickable="true"
-              @click="goToRelation(getName(entity, 'MaterieelDing.beheerder').id)"
-            />
+        <section class="flex flex-col bg-background-light h-auto lg:w-1/3 px-4">
+          <div>
+            <h1 class="text-2xl font-black my-2 text-center lg:text-left lg:ml-6 mt-6">
+              {{ entity.title[0]?.value }}
+            </h1>
+            <div class="m-3 lg:ml-6 lg:mt-6">
+              <base-meta-data :key-word="t('details.modal.objectNumber')" :type="entity.objectNumber[0]?.value" :error-text="t('details.modal.unknown')" />
+              <base-meta-data :key-word="t('details.modal.objectName')" :type="getObjectName(entity.metadataCollection)" :error-text="t('details.modal.unknown')" />
+              <base-meta-data
+                v-if="collectieNaam && collectieNaam.nestedMetaData && collectieNaam.nestedMetaData.title.length != 0"
+                :key-word="t('details.modal.collectieNaam')"
+                :type="collectieNaam.nestedMetaData.title[0].value"
+                :error-text="t('details.modal.unknown')"
+                :clickable="true"
+                @click="goToRelation(collectieNaam.nestedMetaData.id)"
+              />
+              <base-meta-data
+                v-if="getName(entity, 'MaterieelDing.beheerder').name != 'onbekend'"
+                :key-word="t('details.modal.instellingNaam')"
+                :type="getName(entity, 'MaterieelDing.beheerder').name"
+                :error-text="t('details.modal.unknown')"
+                :clickable="true"
+                @click="goToRelation(getName(entity, 'MaterieelDing.beheerder').id)"
+              />
+            </div>
           </div>
-          <div v-if="entity.mediafiles" class="flex flex-row lg:flex-col pr-6 pb-5 overflow-x-auto h-4/5 no-scrollbar">
+          <div v-if="entity.mediafiles" class="flex flex-row lg:flex-col pb-5 overflow-x-auto h-4/5 no-scrollbar">
             <div v-for="(photo, index) in entity.mediafiles" :key="photo">
               <div class="flex relative mb-4 w-60 sm:w-auto">
-                <LazyLoadImage :url="generateUrl(photo.filename, 'full')" extra-class="m-6 sm:w-full" />
+                <LazyLoadImage :url="generateUrl(photo.filename, 'full')" extra-class="my-6 sm:w-full" />
                 <base-button
                   class="w-0 absolute z-20 top-4 left-4 mt-3 ml-3"
                   customStyle="secondary-round"
@@ -55,77 +57,75 @@
             </div>
           </div>
         </section>
-        <section class="flex flex-col flex-grow bg-background-medium w-full h-auto lg:h-full lg:w-2/3 pb-12 lg:pb-0">
-          <div>
-            <p v-show="entity.description && entity.description[0]" class="ml-8 m-3 mt-6 lg:mr-10 xl:mt-20">
-              {{ entity.description[0]?.value }}
-            </p>
-            <!-- <div class="font-medium pb-2 flex flex-wrap">
+        <section class="flex flex-col flex-grow bg-background-medium w-full h-auto lg:h-full lg:w-2/3 pb-12 lg:pb-0 mb-4">
+          <p v-show="entity.description && entity.description[0]" class="ml-8 m-3 mt-6 lg:mr-10 xl:mt-20">
+            {{ entity.description[0]?.value }}
+          </p>
+          <!-- <div class="font-medium pb-2 flex flex-wrap">
             <relation-tag v-for="relation in entity.relations.filter((relation: any) => relation.key)" :id="relation.key" :key="relation.value" class="bg-tag-neutral" />
           </div> -->
-            <div v-if="collectieNaam" class="mt-5 flex flex-col gap-2 mx-8 bg-background-light px-4 py-2">
-              <strong class="col-start-1 w-min inline-block" v-html="t(`${collectieNaam.label}`)" />
-              <a
-                v-if="collectieNaam.nestedMetaData.title.length != 0"
-                class="col-start-1 font-semibold inline-block hover:underline cursor-pointer"
-                v-html="t(`${collectieNaam.nestedMetaData.title[0].value}`)"
-                @click="goToRelation(collectieNaam.nestedMetaData.id)"
-              />
-              <p v-if="collectieNaam.nestedMetaData.description && collectieNaam.nestedMetaData.description.length != 0" class="col-start-1 inline-block mt-2 mb-4">
-                {{ collectieNaam.nestedMetaData.description[0].value }}
-              </p>
-            </div>
+          <div v-if="collectieNaam" class="mt-5 flex flex-col gap-2 mx-8 bg-background-light px-4 py-2">
+            <strong class="col-start-1 w-min inline-block" v-html="t(`${collectieNaam.label}`)" />
+            <a
+              v-if="collectieNaam.nestedMetaData.title.length != 0"
+              class="col-start-1 font-semibold inline-block hover:underline cursor-pointer"
+              v-html="t(`${collectieNaam.nestedMetaData.title[0].value}`)"
+              @click="goToRelation(collectieNaam.nestedMetaData.id)"
+            />
+            <p v-if="collectieNaam.nestedMetaData.description && collectieNaam.nestedMetaData.description.length != 0" class="col-start-1 inline-block mt-2 mb-4">
+              {{ collectieNaam.nestedMetaData.description[0].value }}
+            </p>
+          </div>
 
-            <h3 class="font-bold text-lg mt-5 ml-8">
-              {{ t('details.modal.characteristics') }}
-            </h3>
-            <ul class="mt-5 flex flex-col gap-2 px-8 bg-background-medium">
-              <li v-for="metaType in filterAllData(entity).metadataCollection" :key="metaType">
-                <base-meta-data v-if="!metaType.nested" :key-word="t(`${metaType.label}`)" :type="concatMetadatValues(metaType.data)" :error-text="t('details.modal.unknown')" />
-                <div v-if="metaType.nested" class="mt-2">
-                  <strong class="col-start-1 w-min inline-block" v-html="t(`${metaType.label}`)" />
-                  <li v-for="dataItem in metaType.data" :key="dataItem.value" class="ml-5" :class="dataItem.nestedMetaData.metadataCollection.length === 0 ? 'mb-1' : 'mb-5'">
-                    <strong
-                      v-if="metaType.label !== dataItem.value && dataItem.nestedMetaData.metadataCollection.length === 0"
-                      class="col-start-1 font-semibold inline-block"
-                      v-html="t(`${dataItem.value}`)"
-                    />
-                    <p
-                      v-if="dataItem.nestedMetaData.metadataCollection.length !== 0 && dataItem.nestedMetaData.description && dataItem.nestedMetaData.description[0]"
-                      class="col-start-1 inline-block mt-2 mb-4"
-                    >
-                      {{ dataItem.nestedMetaData.description[0].value ? dataItem.nestedMetaData.description[0].value : '' }}
-                    </p>
-                    <base-meta-data
-                      v-for="(metaData, index) in dataItem.nestedMetaData.metadataCollection"
-                      :key="index"
-                      :strong-label="false"
-                      :key-word="t(`${metaData.label}`)"
-                      :type="concatMetadatValues(metaData.data)"
-                      :error-text="t('details.modal.unknown')"
-                      :clickable="t(`${metaData.label}`) == 'vervaardiger' ? true : false"
-                      @click="t(`${metaData.label}`) == 'vervaardiger' ? goToCreatorDetails(getCreatorId(metaData)) : null"
-                    />
-                  </li>
-                </div>
-              </li>
-            </ul>
-            <div class="bg-background-medium">
-              <h3 class="font-bold text-lg mt-5 mb-3 ml-8">
-                {{ t('details.modal.associations') }}
-              </h3>
-
-              <div class="mx-5 flex gap-3 mb-4 flex-wrap">
-                <div v-for="(relation, index) in entity.types" :key="index">
-                  <a
-                    v-if="relation"
-                    class="px-2 py-2 bg-tag-neutral mb-2 -mr-1 bg-opacity-50 cursor-pointer hover:underline"
-                    :key="relation.id"
-                    @click=";`${goToRelation(relation.id.replace('entities/', ''))}`"
+          <h3 class="font-bold text-lg mt-5 ml-8">
+            {{ t('details.modal.characteristics') }}
+          </h3>
+          <ul class="mt-5 flex flex-col gap-2 px-8 bg-background-medium">
+            <li v-for="metaType in filterAllData(entity).metadataCollection" :key="metaType">
+              <base-meta-data v-if="!metaType.nested" :key-word="t(`${metaType.label}`)" :type="concatMetadatValues(metaType.data)" :error-text="t('details.modal.unknown')" />
+              <div v-if="metaType.nested" class="mt-2">
+                <strong class="col-start-1 w-min inline-block" v-html="t(`${metaType.label}`)" />
+                <li v-for="dataItem in metaType.data" :key="dataItem.value" class="ml-5" :class="dataItem.nestedMetaData.metadataCollection.length === 0 ? 'mb-1' : 'mb-5'">
+                  <strong
+                    v-if="metaType.label !== dataItem.value && dataItem.nestedMetaData.metadataCollection.length === 0"
+                    class="col-start-1 font-semibold inline-block"
+                    v-html="t(`${dataItem.value}`)"
+                  />
+                  <p
+                    v-if="dataItem.nestedMetaData.metadataCollection.length !== 0 && dataItem.nestedMetaData.description && dataItem.nestedMetaData.description[0]"
+                    class="col-start-1 inline-block mt-2 mb-4"
                   >
-                    {{ relation.label }}
-                  </a>
-                </div>
+                    {{ dataItem.nestedMetaData.description[0].value ? dataItem.nestedMetaData.description[0].value : '' }}
+                  </p>
+                  <base-meta-data
+                    v-for="(metaData, index) in dataItem.nestedMetaData.metadataCollection"
+                    :key="index"
+                    :strong-label="false"
+                    :key-word="t(`${metaData.label}`)"
+                    :type="concatMetadatValues(metaData.data)"
+                    :error-text="t('details.modal.unknown')"
+                    :clickable="t(`${metaData.label}`) == 'vervaardiger' ? true : false"
+                    @click="t(`${metaData.label}`) == 'vervaardiger' ? goToCreatorDetails(getCreatorId(metaData)) : null"
+                  />
+                </li>
+              </div>
+            </li>
+          </ul>
+          <div class="bg-background-medium">
+            <h3 class="font-bold text-lg mt-5 mb-3 ml-8">
+              {{ t('details.modal.associations') }}
+            </h3>
+
+            <div class="mx-5 flex gap-3 mb-4 flex-wrap">
+              <div v-for="(relation, index) in entity.types" :key="index">
+                <a
+                  v-if="relation"
+                  class="px-2 py-2 bg-tag-neutral mb-2 -mr-1 bg-opacity-50 cursor-pointer hover:underline"
+                  :key="relation.id"
+                  @click=";`${goToRelation(relation.id.replace('entities/', ''))}`"
+                >
+                  {{ relation.label }}
+                </a>
               </div>
             </div>
           </div>
@@ -133,7 +133,7 @@
         </section>
       </section>
     </section>
-    <section id="footer" class="flex items-center z-50 bg-background-light justify-center p-2 lg:p-10 h-1/6 shadow-2xl sticky bottom-0 w-full lg:pb-6">
+    <section id="footer" class="flex items-center z-50 bg-background-light justify-center p-2 lg:p-10 shadow-2xl sticky bottom-0 w-full lg:pb-6">
       <base-button
         class="w-12 h-12 pl-6 ml-3 stroke-current text-text-black inline-block lg:hidden"
         :on-click="() => copyUrl(entity.id)"
