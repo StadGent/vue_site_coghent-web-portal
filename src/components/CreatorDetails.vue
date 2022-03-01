42af6e077cacc9d8d2b2d87a764fa9df125e1392
Berend <berend@inuits.eu>
2022-03-01 14:47:10 +0100

    Refs #90238 Add gender to creator page

--- src/components/CreatorDetails.vue
+++ src/components/CreatorDetails.vue
@@ -1,32 +1,40 @@
 <template>
-<div v-if="result" class="w-full overflow-x-hidden">
-  <div class="grid sm:grid-cols-2 sm:gap-3 h-auto w-full px-3">
-    <div v-if="person" class="pb-0 sm:pb-24">
-      <section>
-        <div>
-          <base-button v-if="route.query.fromPage" class="inline⁻block w-full" :text="t('creator.goBack')+ ' ' + route.query.fromPage" custom-style="details-black" custom-icon="arrowLeftLine" :icon-shown="true" @click="goToPreviousPage" />
+  <div v-if="result" class="w-full overflow-x-hidden">
+    <div class="grid sm:grid-cols-2 sm:gap-3 h-auto w-full px-3">
+      <div v-if="person" class="pb-0 sm:pb-24">
+        <section>
+          <div>
+            <base-button
+              v-if="route.query.fromPage"
+              class="inline⁻block w-full"
+              :text="t('creator.goBack') + ' ' + route.query.fromPage"
+              custom-style="details-black"
+              custom-icon="arrowLeftLine"
+              :icon-shown="true"
+              @click="goToPreviousPage"
+            />
+          </div>
+          <h1 class="md:text-5xl sm:text-4xl text-3xl font-bold w-full py-10 block leading-normal">{{ person.fullName }}</h1>
+        </section>
+        <div class="w-screen">
+          <base-meta-data-tab :tab-names="tabs" :hidden="true" />
         </div>
-        <h1 class="md:text-5xl sm:text-4xl text-3xl font-bold w-full py-10 block leading-normal">{{person.fullName}}</h1>
-      </section>
-      <div class="w-screen">
-          <base-meta-data-tab :tab-names="tabs" :hidden="true"/>
-        </div>
-      <section v-if="person.extraInfo.length">
-        <base-meta-data v-for="(info, index) in person.extraInfo" :key="index" :key-word="t('creator.' + info.key)" :type="info.value" :error-text="t('details.modal.unknown')" class="w-full" />
-      </section>
-      <section v-else>
-        <p>{{t('creator.noMetadata')}}</p>
-      </section>
+        <section v-if="person.extraInfo.length">
+          <base-meta-data v-for="(info, index) in person.extraInfo" :key="index" :key-word="t('creator.' + info.key)" :type="info.value" :error-text="t('details.modal.unknown')" class="w-full" />
+        </section>
+        <section v-else>
+          <p>{{ t('creator.noMetadata') }}</p>
+        </section>
+      </div>
+      <div class="w-full justify-center items-center">
+        <img src="" class="w-auto" />
+      </div>
     </div>
-    <div class="w-full justify-center items-center">
-      <img src="" class="w-auto" />
+    <div class="flex flex-col w-full px-3">
+      <h2 class="md:text-5xl sm:text-4xl text-3xl font-bold w-full text-center py-10 block leading-normal">{{ t('creator.moreWorks') + ' ' + person.fullName }}</h2>
+      <the-grid :small="true" :no-header="true" class="sm:mt-24" :defaultRelations="['entities/' + id]" :noFilters="true" />
     </div>
   </div>
-  <div class="flex flex-col w-full px-3">
-  <h2 class="md:text-5xl sm:text-4xl text-3xl font-bold w-full text-center py-10 block leading-normal">{{t('creator.moreWorks') + ' ' + person.fullName}}</h2>
-<the-grid :small="true" :no-header="true" class="sm:mt-24" :defaultRelations="['entities/' + id]" :noFilters="true"/>
-  </div>
-</div>
 </template>
 
 <script lang="ts">
@@ -36,17 +44,18 @@ import { useI18n } from 'vue-i18n'
 import { useRoute, useRouter } from 'vue-router'
 import { useQuery } from '@vue/apollo-composable'
 import { GetCreatorByIdDocument, GetEntityByIdDocument, BaseButton, BaseMetaData, BaseMetaDataTab, FullRelationFragment } from 'coghent-vue-3-component-library'
+import { Relation, Entity } from 'coghent-vue-3-component-library/lib/queries'
 
 const asString = (x: string | string[]) => (Array.isArray(x) ? x[0] : x)
 
 export type Metadata = {
-  key:string
-  value:string
+  key: string
+  value: string
 }
 
 export type Person = {
-  fullName?:string
-  extraInfo?:Metadata[]
+  fullName?: string
+  extraInfo?: Metadata[]
 }
 
 export default defineComponent({
@@ -55,7 +64,7 @@ export default defineComponent({
     BaseButton,
     BaseMetaData,
     BaseMetaDataTab,
-    TheGrid
+    TheGrid,
   },
   props: {},
   setup: () => {
@@ -75,9 +84,9 @@ export default defineComponent({
     }
 
     const getNameFromMetadata = (key: string) => {
-      if (metadata.value){
-        const foundItem = metadata.value.find(element => element.key == key)
-        metadata.value = metadata.value.filter(element => element != foundItem)
+      if (metadata.value) {
+        const foundItem = metadata.value.find((element) => element.key == key)
+        metadata.value = metadata.value.filter((element) => element != foundItem)
         return foundItem?.value
       }
     }
@@ -86,60 +95,72 @@ export default defineComponent({
       const objects: Array<Metadata> = []
       entity.metadataCollection.forEach((collection: any) => {
         const dataObject: Metadata = {
-        key: entity.type + '_' + collection.label,
-        value: collection.data[0].value,
-      }
+          key: entity.type + '_' + collection.label,
+          value: collection.data[0].value,
+        }
         objects.push(dataObject)
-      });
+      })
       return objects
+    }
+
+    const getDataFromRelations = (relations: Relation[], relationLabels: string[]) => {
+      relations.forEach((relation) => {
+        if (relationLabels.find((relationlabel: string) => relationlabel == relation.label) && relation.label && relation.value) {
+          const dataObject: Metadata = {
+            key: relation.label,
+            value: relation.value,
+          }
+          if (person.value?.extraInfo) {
+            person.value?.extraInfo.push(dataObject)
+          } else if (person.value) {
+            person.value.extraInfo = [dataObject]
+          }
+        }
+      })
     }
 
     onResult((queryResult: any) => {
       if (!queryResult.error) {
         const tempMetadata: Array<Metadata> = queryResult.data.Entity?.metadata
-        metadata.value = tempMetadata.filter(item => item.value != '')
+        metadata.value = tempMetadata.filter((item) => item.value != '')
         person.value = {
           fullName: getNameFromMetadata('fullname'),
-          extraInfo:metadata.value
+          extraInfo: metadata.value,
         }
-        
-        queryResult.data.Entity?.relations
-          .forEach((relation: any) => {
-            if (relation.label == "heeftGeboorte" || relation.label == "heeftOverlijden"){
-              additionalInfoIds.push(relation.key)
-            }
-          })
-          
-        if(additionalInfoIds){
-          additionalInfoIds.forEach(url => {
+
+        getDataFromRelations(queryResult.data.Entity.relations, ['geslacht'])
+
+        queryResult.data.Entity?.relations.forEach((relation: Relation) => {
+          if (relation.label == 'heeftGeboorte' || relation.label == 'heeftOverlijden') {
+            additionalInfoIds.push(relation.key)
+          }
+        })
+
+        if (additionalInfoIds) {
+          additionalInfoIds.forEach((url) => {
             const id = url.replace('entities/', '')
-            additionalMetaDataRefetch({id})
-          });
+            additionalMetaDataRefetch({ id })
+          })
         }
-        
-      }
-      else{
-          router.push('/entity/not-found')
+      } else {
+        router.push('/entity/not-found')
       }
     })
 
-  onAdditionalMetaDataResult((queryResult) => {
-    if (queryResult.data?.Entity){
-      const entity = queryResult.data.Entity
-      const infoArray: Array<Metadata> = createObjectFromAdditionalData(entity)
+    onAdditionalMetaDataResult((queryResult) => {
+      if (queryResult.data?.Entity) {
+        const entity = queryResult.data.Entity
+        const infoArray: Array<Metadata> = createObjectFromAdditionalData(entity)
 
-      infoArray.forEach((info: Metadata) => {
-        if (person.value?.extraInfo){
-          person.value?.extraInfo.push(info)
-        }
-        else{
-          person.value = {
-          extraInfo:metadata.value
-        }
-        }
-      })
-    }
-  })
+        infoArray.forEach((info: Metadata) => {
+          if (person.value?.extraInfo) {
+            person.value?.extraInfo.push(info)
+          } else if (person.value) {
+            person.value.extraInfo = metadata.value
+          }
+        })
+      }
+    })
 
     return {
       t,
@@ -148,7 +169,7 @@ export default defineComponent({
       route,
       person,
       result,
-      id
+      id,
     }
   },
 })
