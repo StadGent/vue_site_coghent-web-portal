8f5d454427ecdde3d26dedb32eca154092b5e703
Berend <berend@inuits.eu>
2022-04-25 10:31:00 +0200

    fix for suprise button & get frames from activebox query

--- src/components/TheGrid.vue
+++ src/components/TheGrid.vue
@@ -42,7 +42,7 @@
 <script lang="ts">
 import { defineComponent, watch, ref, reactive, PropType, computed, toRaw, onMounted } from 'vue'
 import { useQuery } from '@vue/apollo-composable'
-import { BaseSearch, GetEntitiesDocument, GetEntitiesQuery, GetEntitiesQueryVariables, BaseButton, TheMasonry } from 'coghent-vue-3-component-library'
+import { BaseSearch, GetEntitiesDocument, GetEntitiesQuery, GetEntitiesQueryVariables, BaseButton, TheMasonry, GetActiveBoxDocument } from 'coghent-vue-3-component-library'
 import 'coghent-vue-3-component-library/lib/index.css'
 import { useI18n } from 'vue-i18n'
 import Filter from './Filter.vue'
@@ -100,44 +100,42 @@ export default defineComponent({
     const { randomValue, refresh: refreshSeed } = useSeed()
     const { generateUrl, noImageUrl } = useIIIF()
     const { clearHistory } = useHistory()
-    let frameList: string[] = [
-      'entities/df39c7c0-7be2-4ba3-935a-fdaa04443d00',
-      'entities/a408b8f5-3517-46e9-8572-25b4e1a7cb12',
-      'entities/28db711b-8f0a-41bb-85b3-06449d51eae6',
-      'entities/edd2f296-3b60-4963-8061-f41ded2d726f',
-      'entities/914a91e6-c213-4f13-8e38-313be0502f93',
-      'entities/c513d5db-53f4-419d-b9d5-199ce54cd3bc',
-      'entities/ab13db4c-5a04-410a-898a-6e79a3a3ea74',
-      'entities/03a4e320-8bc0-4a89-b804-e410af40befa',
-      'entities/8d1ec60d-6546-4372-a914-1ec247c57a01',
-      'entities/fffe30bd-fa8f-4fbe-891f-b430c7ddcfa6',
-      'entities/4fe58733-9787-4e5b-9509-ce3ba6634cee',
-      'entities/0d00a910-9beb-436a-a842-46890902aae0',
-      'entities/b030ebc5-4d3c-4b52-be56-3e2f7dee849d',
-      'entities/2165a1c8-6e4d-479c-9ba0-5cf28e4b9214',
-      'entities/23a8b928-c92f-46e4-8738-51053b0c7659',
-      'entities/425c5312-8453-4398-8609-2fa7cd79ec27',
-      'entities/4097e14d-fcab-401b-b659-1b292fefee5c',
-      'entities/867f65e9-5fa8-4319-a967-d8707a1ab7c6',
-      'entities/e3645039-08ee-4ce6-b572-43e6c5405359',
-      'entities/d3c38a38-168f-47df-b51d-06f06cdef95a',
-    ]
+    const frameList = ref<string[]>([])
 
     const getSelectedFilters = computed<string[]>(() => {
       if (props.defaultRelations?.length > 0 && selectedFilters.value.length === 0) {
         return props.defaultRelations
       } else if (route.query.touch) {
-        return frameList
+        return frameList.value
       }
       return selectedFilters.value
     })
+
+    if (route.query.touch) {
+      const { result: activeBoxResult, loading: loadingActiveBoxResult } = useQuery(GetActiveBoxDocument)
+
+      watch(
+        () => activeBoxResult.value,
+        (boxResult) => {
+          if (boxResult?.ActiveBox.results) {
+            const frameIds: string[] = []
+            boxResult.ActiveBox.results.forEach((story: any) => {
+              const storyFrameIds: string[] = story.frames.map((frame: Entity) => 'entities/' + frame.id)
+              frameIds.push(...storyFrameIds)
+            })
+            frameList.value.push(...frameIds)
+            refetch()
+          }
+        }
+      )
+    }
 
     const { result, loading, fetchMore, onResult, refetch } = useQuery<GetEntitiesQuery, GetEntitiesQueryVariables>(
       GetEntitiesDocument,
 
       () => ({
         limit: limit,
-        skip: (_skip = 0),
+        skip: _skip,
         searchValue: {
           value: searchQueryForQuery.value,
           isAsc: false,
@@ -187,26 +185,14 @@ export default defineComponent({
     }
 
     const resetQuery = () => {
+      frameList.value = []
       refreshSeed()
       if (masonry.value && masonry.value.contructTiles) {
         masonry.value.contructTiles(limit, true)
       }
       selectedFilters.value = []
       if (searchQueryForQuery.value === '') {
-        refetch({
-          limit: limit,
-          skip: (_skip = 0),
-          searchValue: {
-            value: searchQueryForQuery.value,
-            isAsc: false,
-            relation_filter: [],
-            randomize: getSelectedFilters.value.length > 0 || searchQueryForQuery.value !== '' ? false : true,
-            seed: randomValue.value,
-            key: 'title',
-            has_mediafile: true,
-            skip_relations: props.noRelations ? props.noRelations : false,
-          },
-        })
+        refetch()
       } else {
         searchQueryForInput.value = ''
         searchQueryForQuery.value = ''
