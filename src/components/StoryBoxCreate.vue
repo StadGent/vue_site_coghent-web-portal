cf1617b5ec3892db04a067e0b2548db5b0634041
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2022-05-20 10:52:45 +0200

    Can drag and drop assets in the list (only one level at a time)

--- src/components/StoryBoxCreate.vue
+++ src/components/StoryBoxCreate.vue
@@ -5,25 +5,24 @@
         <h1 class="text-lg my-2 font-bold">{{ t('storybox.assets.title') }}</h1>
         <div>
           <p class="text-sm">{{ t('storybox.assets.selectedAssetsInfo') }}</p>
-          <ul v-if="assets.length > 0" class="overflow-y-auto">
-            <li v-for="asset in assets" :key="asset" class="w-full my-2 align-middle">
-              <div class="w-full bg-background-medium flex flex-cols py-2">
-                <p class="w-20 flex items-center justify-center cursor-grab focus:cursor-grabbing">
-                  <base-icon :icon="'dragAndDrop'" class="stroke-current" />
+          <ul v-if="assets.length > 0" class="overflow-y-auto" :ondragenter="dragEnter">
+            <li v-for="asset in assets" :key="asset" class="w-full my-2 align-middle" :id="asset.id" :ondragstart="dragStart" :ondrop="drop" :draggable="canDrag">
+              <div class="w-full bg-background-medium flex flex-cols py-2" :id="asset.id">
+                <p @mousedown="activateDrag(true)" @mouseleave="activateDrag(false)" :id="asset.id" class="w-20 flex items-center justify-center cursor-move">
+                  <base-icon :id="asset.id" :icon="'dragAndDrop'" class="stroke-current" />
                 </p>
-                <div class="w-20 flex justify-center items-center">
-                  <img class="w-16 h-16 object-scale-down" :src="asset.mediafiles[0].thumbnail_file_location" />
+                <div :id="asset.id" class="w-20 flex justify-center items-center">
+                  <img :id="asset.id" class="w-16 h-16 object-scale-down" :src="asset.mediafiles[0].thumbnail_file_location" />
                 </div>
-                <!-- <p class="w-16 flex items-center justify-center">Thumb</p> -->
-                <div class="flex flex-col w-full px-2">
-                  <h1 class="text-lg font-bold">{{ asset.title[0] ? asset.title[0].value : 'asset' }}</h1>
-                  <p class="text-sm">{{ asset.description[0] && asset.description[0].value != '' ? asset.description[0].value.substr(0, 50) + '..' : '' }}</p>
+                <div :id="asset.id" class="flex flex-col w-full px-2">
+                  <h1 :id="asset.id" class="text-lg font-bold">{{ asset.title[0] ? asset.title[0].value : 'asset' }}</h1>
+                  <p :id="asset.id" class="text-sm">{{ asset.description[0] && asset.description[0].value != '' ? asset.description[0].value.substr(0, 50) + '..' : '' }}</p>
                 </div>
-                <div class="w-20 flex items-center justify-center items-row-reverse cursor-pointer">
-                  <base-icon :icon="'info'" @click="() => router.push(`/entity/${asset.id}`)" class="stroke-current" />
+                <div :id="asset.id" class="w-20 flex items-center justify-center items-row-reverse cursor-pointer">
+                  <base-icon :id="asset.id" :icon="'info'" @click="() => router.push(`/entity/${asset.id}`)" class="stroke-current" />
                 </div>
-                <div class="w-20 flex items-center justify-center items-row-reverse cursor-pointer" @click="deleteAsset(asset)">
-                  <base-icon :icon="'wasteBasket'" class="stroke-current" />
+                <div :id="asset.id" class="w-20 flex items-center justify-center items-row-reverse cursor-pointer" @click="deleteAsset(asset)">
+                  <base-icon :id="asset.id" :icon="'wasteBasket'" class="stroke-current" />
                 </div>
               </div>
             </li>
@@ -47,7 +46,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType, ref } from 'vue'
+import { defineComponent, PropType, ref, watch } from 'vue'
 import { BaseIcon } from 'coghent-vue-3-component-library'
 import { Entity } from 'coghent-vue-3-component-library'
 import { StoryBuild } from '@/pages/TheStoryboxPage.vue'
@@ -55,6 +54,7 @@ import { useI18n } from 'vue-i18n'
 import { useBoxVisiter } from 'coghent-vue-3-component-library'
 import { apolloClient, router } from '@/app'
 import useStoryBox from '@/composables/useStoryBox'
+
 export default defineComponent({
   components: { BaseIcon },
   props: {
@@ -71,10 +71,8 @@ export default defineComponent({
   setup(props, { emit }) {
     const { t } = useI18n()
     const { getRelationEntities } = useStoryBox()
-
-    const updateAssets = (_assets: Array<typeof Entity>) => {
-      emit(`assets`, _assets)
-    }
+    const startDragItem = ref<string>('')
+    const canDrag = ref<boolean>(false)
 
     const deleteAsset = async (_asset: typeof Entity) => {
       await useBoxVisiter(apolloClient).deleteRelationFromBoxVisiter('31099546', _asset.id)
@@ -87,7 +85,37 @@ export default defineComponent({
       emit(`description`, description)
     }
 
-    return { t, updateAssets, updateDescription, deleteAsset, router }
+    watch(canDrag, () => console.log(`candrag`, canDrag.value))
+
+    const dragStart = (event: any) => {
+      startDragItem.value = event.srcElement.id
+    }
+
+    const dragEnter = (event: any) => {
+      event.preventDefault()
+      swap(props.assets, startDragItem.value, event.srcElement.id)
+    }
+    const swap = (_assets: Array<typeof Entity>, itemOne: string, itemTwo: string) => {
+      const assetIndexOne = _assets.indexOf(_assets[_assets.map((asset) => asset.id === itemOne).indexOf(true)])
+      const assetOne = _assets[_assets.map((asset) => asset.id === itemOne).indexOf(true)]
+      const assetIndexTwo = _assets.indexOf(_assets[_assets.map((asset) => asset.id === itemTwo).indexOf(true)])
+      const assetTwo = _assets[_assets.map((asset) => asset.id === itemTwo).indexOf(true)]
+      if (canDrag.value === true && assetOne && assetTwo && assetIndexOne != assetIndexTwo) {
+        let updatedAssets: Array<typeof Entity> = []
+        Object.assign(updatedAssets, _assets)
+        updatedAssets[assetIndexOne] = assetTwo
+        updatedAssets[assetIndexTwo] = assetOne
+        emit(`assets`, updatedAssets)
+      }
+    }
+
+    const activateDrag = (activate: boolean) => {
+      console.log(`candrag activate`, activate)
+      console.log(`candrag activate`, canDrag.value)
+      canDrag.value = activate
+    }
+
+    return { t, updateDescription, deleteAsset, router, dragStart, dragEnter, canDrag, activateDrag }
   },
 })
 </script>
