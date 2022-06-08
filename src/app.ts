676d9b7ae9178b327fc7949cecd97c9c0a90f72b
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2022-06-08 09:33:56 +0200

    Update session to 0.1.5 (trail error link with session-vue-library)

--- src/app.ts
+++ src/app.ts
@@ -28,6 +28,7 @@ export const useStoryboxFeature = ref<boolean>(false)
 //
 
 export default async function (authenticated: boolean) {
+  console.log(`>web-portal updated session to 0.1.5`)
   const configStore = StoreFactory.get(ConfigStore)
   const config = await fetch('../config.json').then((r) => r.json())
   configStore.setConfig(config)
