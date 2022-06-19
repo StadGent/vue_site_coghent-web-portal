e12366c606850bc0be98dfcad274d0173a65c101
Berend <berend@inuits.eu>
2022-06-19 15:26:51 +0200

    Page for linking touchtable box to account

--- src/pages/TheVisitPage.vue
+++ src/pages/TheVisitPage.vue
@@ -1,20 +1,35 @@
 <template>
-  <div class="w-screen h-screen"></div>
+  <div class="flex flex-col items-center justify-center w-full">
+    <BaseIcon class="black p-12 opacity-50" icon="link" :width="250" :height="250" /><BoxVisitCode :code="code" />
+    <div class="flex items-center mt-12">
+      <CircleLoader />
+      <p>{{ t('visit.linkCodeToAccount') }}</p>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import { useRoute } from 'vue-router'
+import { BoxVisitCode, BaseIcon, CircleLoader } from 'coghent-vue-3-component-library'
+import { useI18n } from 'vue-i18n'
 
 export default defineComponent({
   name: 'TheVisitPage',
-  components: {},
+  components: { BoxVisitCode, BaseIcon, CircleLoader },
   props: {},
   setup() {
+    const { t } = useI18n()
     const route = useRoute()
-    console.log(route.params.visitCode)
+    const code = ref<string | string[]>(route.params.visitCode)
 
-    return {}
+    return { t, code }
   },
 })
 </script>
+
+<style scoped>
+.black {
+  stroke: black;
+}
+</style>
