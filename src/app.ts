854b5e1a9ff3f68f8bd162a10db924eb7dfb5f16
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2022-07-11 13:56:51 +0200

    Bump cl to 0.1.99

--- src/app.ts
+++ src/app.ts
@@ -34,7 +34,7 @@ export const useGtmFeature = ref<boolean>(false)
 
 export default async function (authenticated: boolean) {
   console.log(`>web-portal updated session to v0.1.7`)
-  console.log(`>web-portal updated CL to v0.1.98`)
+  console.log(`>web-portal updated CL to v0.1.99`)
   const configStore = StoreFactory.get(ConfigStore)
   const config = await fetch('../config.json').then((r) => r.json())
   configStore.setConfig(config)
