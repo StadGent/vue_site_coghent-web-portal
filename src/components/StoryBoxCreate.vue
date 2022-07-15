816ecebadb611525a74d613c11ff47d744d50b74
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2022-07-15 13:57:02 +0200

    Refs #97220 the duration of the asset is set in the parent card No more dropdown

--- src/components/StoryBoxCreate.vue
+++ src/components/StoryBoxCreate.vue
@@ -44,16 +44,7 @@
                 <div :id="asset.id" class="invisible w-20 flex items-center justify-center items-row-reverse cursor-pointer">
                   <base-icon :id="asset.id" :icon="'info'" class="stroke-current" @click="() => router.push(`/entity/${asset.id}`)" />
                 </div>
-                <div :id="asset.id" class="w-28 flex items-center justify-center items-row-reverse cursor-pointer" @click="showTimingEdit(asset.id)">
-                  <base-icon :id="asset.id" :icon="'edit'" class="stroke-current" />
-                </div>
-                <div :id="asset.id" class="w-28 flex items-center justify-center items-row-reverse cursor-pointer" @click="deleteAsset(asset)">
-                  <base-icon :id="asset.id" :icon="'wasteBasket'" class="stroke-current" />
-                </div>
-              </div>
-              <div v-if="showTimeEdit === true && activeEditItem === asset.id" :id="'expand' + asset.id" class="bg-background-light flex flex-row px-4 gap-4 p-2 pt-6">
-                <label class="flex flex-row text-bold items-center" for="duration">
-                  Deze afbeelding wordt getoond voor
+                <div :id="asset.id" class="w-28 flex items-center justify-center items-row-reverse cursor-pointer">
                   <BaseDropDown
                     :number-step="5"
                     :number-max="60"
@@ -61,8 +52,10 @@
                     :style="`p-1.5 rounded-md ml-2 w-16 mr-2 bg-text-white`"
                     @selected="(option) => updateAssetTiming(asset, option)"
                   />
-                  seconden.
-                </label>
+                </div>
+                <div :id="asset.id" class="w-28 flex items-center justify-center items-row-reverse cursor-pointer" @click="deleteAsset(asset)">
+                  <base-icon :id="asset.id" :icon="'wasteBasket'" class="stroke-current" />
+                </div>
               </div>
             </div>
             <div v-if="draggingAssetComesBelow === asset.id && draggingAssetComesBelow != startDragItem && canDrag === true" class="my-1.5 h-1 bg-background-dark w-full"></div>
@@ -106,12 +99,10 @@ export default defineComponent({
       required: true,
     },
   },
-  setup(props) {
+  setup() {
     const { t } = useI18n()
     const startDragItem = ref<string>('')
     const canDrag = ref<boolean>(false)
-    const showTimeEdit = ref<boolean>(false)
-    const activeEditItem = ref<string | null>(null)
     const assets = ref<Array<typeof Entity>>(StoryBoxState.value.activeStorybox.assets)
     const assetTimings = ref<Array<typeof Entity>>(StoryBoxState.value.activeStorybox.assetTimings)
     const draggingAssetComesBelow = ref<string | null>(null)
@@ -135,7 +126,6 @@ export default defineComponent({
     }
 
     const dragStart = (event: any) => {
-      showTimeEdit.value = false
       startDragItem.value = event.srcElement.id
       document.getElementById(event.srcElement.id)?.parentElement?.classList.add(`test`)
       document.getElementById(event.srcElement.id)?.firstElementChild?.classList.replace('bg-background-light', 'bg-background-dark')
@@ -154,9 +144,7 @@ export default defineComponent({
       event.preventDefault()
       swap(StoryBoxState.value.activeStorybox.assets, startDragItem.value, event.srcElement.id)
     }
-    const click = (event: any) => {
-      console.log(event)
-    }
+
     const swap = async (_assets: Array<typeof Entity>, itemOne: string, itemTwo: string) => {
       const assetIndexOne = _assets.indexOf(_assets[_assets.map((asset) => asset.id === itemOne).indexOf(true)])
       const assetOne = _assets[_assets.map((asset) => asset.id === itemOne).indexOf(true)]
@@ -193,17 +181,6 @@ export default defineComponent({
       return updatedItems
     }
 
-    const showTimingEdit = (_assetId: string) => {
-      if (activeEditItem.value === _assetId && showTimeEdit.value === true) showTimeEdit.value = false
-      else {
-        showTimeEdit.value = true
-        activeEditItem.value = null
-        activeEditItem.value = _assetId
-        const doc = document.getElementById(`expand${_assetId}`)
-        doc?.classList.add(`expanding`)
-      }
-    }
-
     const setAssetTiming = (_asset: typeof Entity) => {
       let returnValue = null
       StoryBoxState.value.activeStorybox.assetTimings.map((_pair: typeof KeyValuePair) => {
@@ -229,10 +206,6 @@ export default defineComponent({
       dragEnd,
       dragEnter,
       canDrag,
-      click,
-      showTimeEdit,
-      showTimingEdit,
-      activeEditItem,
       setAssetTiming,
       updateAssetTiming,
       StoryBoxState,
