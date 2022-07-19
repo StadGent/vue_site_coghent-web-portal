91072ca51409c308b4bfe4b10c4a5323d0560286
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2022-07-19 16:13:30 +0200

    Refs #85600 get the relations of the search

--- src/components/UploadStepThree.vue
+++ src/components/UploadStepThree.vue
@@ -5,10 +5,11 @@
       <div class="bg-text-white py-4 px-8 flex flex-col text-left">
         <h2 class="font-bold text-lg mb-4">{{ t('myWorks.upload.stepThree.metadata.title') }}</h2>
         <span class="pr-4 overflow-y-auto">
-          <div class="flex flex-col my-4" v-for="question of questions" :key="question.text">
+          <div class="flex flex-col my-4" v-for="question of metadata" :key="question.text">
             <label class="block text-base font-normal mb-2" :for="question.text"> {{ question.text }} </label>
             <input
-              :v-modal="question.answer"
+              @update="(event) => (question.answer = event.target.value)"
+              :value="question.answer"
               :id="question.text"
               class="bg-background-light appearance-none rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               type="text"
@@ -18,45 +19,128 @@
       </div>
       <div class="bg-text-white py-4 px-8 flex flex-col text-left">
         <h2 class="font-bold text-lg">{{ t('myWorks.upload.stepThree.relation.title') }}</h2>
+        <input
+          v-model="relationSearch"
+          class="mt-8 bg-background-light appearance-none rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          type="text"
+          :placeholder="t(`myWorks.upload.stepThree.relation.search`)"
+        />
+        <h3 class="mt-8 text-base font-normal">{{ t(`myWorks.upload.stepThree.relation.relations`) }}</h3>
+        <div class="flex-grow my-4 p-4 flex flex-row flex-wrap bg-background-light gap-2">
+          <div v-for="tag of relations" :key="tag" class="mr-2 bg-tag-neutral max-h-8 w-fit flex items-center px-2 py-1">
+            <span>tag</span> <BaseIcon icon="check" class="stroke-current ml-2 p-1" />
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { useQuery } from '@vue/apollo-composable'
+import { Relation } from 'coghent-vue-3-component-library'
+import { GetEntitiesDocument } from 'coghent-vue-3-component-library'
+import { MetaKey, BaseIcon } from 'coghent-vue-3-component-library'
+import { defineComponent, ref, watch } from 'vue'
 import { useI18n } from 'vue-i18n'
+import { debounce } from 'ts-debounce'
 
-type Question = {
+type MetadataQuestion = {
   text: string
   answer: string | null
+  key: typeof MetaKey | null
 }
 
 export default defineComponent({
-  components: {},
+  components: {
+    BaseIcon,
+  },
   setup() {
     const { t } = useI18n()
-    const questions = ref<Array<Question>>([])
+    const metadata = ref<Array<MetadataQuestion>>([])
+    const relations = ref<Array<typeof Relation>>([])
+    const relationSearch = ref<string>('')
+    const searchValue = ref<string | null>(null)
+    const queryRelations = ref<boolean>(true)
 
-    const setQuestions = () => {
+    const { result, onResult, loading, fetchMore } = useQuery(
+      GetEntitiesDocument,
+      () => ({
+        limit: 10,
+        skip: 0,
+        searchValue: {
+          value: '',
+          isAsc: false,
+          relation_filter: [],
+          randomize: true,
+          // seed: randomValue.value,
+          key: 'title',
+          has_mediafile: true,
+          skip_relations: false,
+          and_filter: true,
+        },
+      }),
+      () => ({
+        prefetch: false,
+        enabled: queryRelations.value,
+      })
+    )
+
+    onResult((response) => {
+      console.log(`response`, response)
+      queryRelations.value = false
+    })
+
+    watch(relationSearch, async (value) => {
+      searchValue.value = null
+      if (loading.value === false && queryRelations.value === false) {
+        searchValue.value = await debounce(() => relationSearch.value, 3000)()
+        await fetchMore({
+          variables: {
+            limit: 10,
+            skip: 0,
+            searchValue: {
+              value: searchValue.value,
+              isAsc: false,
+              relation_filter: [],
+              randomize: true,
+              // seed: randomValue.value,
+              key: 'title',
+              has_mediafile: true,
+              skip_relations: false,
+              and_filter: true,
+            },
+          },
+        })
+        queryRelations.value = true
+      }
+    })
+    watch(searchValue, async (value) => {
+      console.log(`searchValue`, value)
+    })
+
+    const setMetadataQuestions = () => {
       for (let index = 1; index <= 4; index++) {
-        questions.value.push({
+        metadata.value.push({
           text: t(`myWorks.upload.stepThree.metadata.q${index}`),
           answer: null,
+          key: null,
         })
       }
-      console.log(`questions SET`, questions.value)
     }
 
     const init = () => {
-      setQuestions()
+      searchValue.value = null
+      setMetadataQuestions()
     }
 
     init()
 
     return {
       t,
-      questions,
+      metadata,
+      relations,
+      relationSearch,
     }
   },
 })
