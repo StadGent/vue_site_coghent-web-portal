865cb7a1b8a438f82b5ef7b25538208561b4a4d0
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2022-07-20 10:19:29 +0200

    Added metadata to the summary

--- src/components/UploadStepFour.vue
+++ src/components/UploadStepFour.vue
@@ -2,12 +2,15 @@
   <div class="m-8 h-full flex text-center items-center flex-col">
     <h1 class="flex flex-wrap font-bold text-4xl">{{ t('myWorks.upload.stepFour.title') }}</h1>
     <div class="w-full h-full flex flex-col py-8 justify-center items-center">
-      <div class="w-3/4 bg-text-white py-4 px-8 flex flex-row text-left">
+      <div class="w-4/5 bg-text-white py-4 px-8 flex flex-row text-left">
         <div class="w-2/5 flex flex-grow justify-center items-center py-8">
-          <img src="/no-image.png" alt="">
+          <img src="/no-image.png" alt="" />
         </div>
-        <div class="w-3/5 flex flex-grow text-left">
-          
+        <div class="w-3/5 flex flex-grow flex-col text-left p-8">
+          <div class="flex flex-col mb-4" v-for="meta of uploadState.metadata" :key="meta.key">
+            <p class="text-base font-normal mb-2 text-touchtable-dark opacity-50">{{ meta.label }}</p>
+            <h4 class="font-semibold text-lg">{{ meta.value }}</h4>
+          </div>
         </div>
       </div>
     </div>
@@ -15,33 +18,22 @@
 </template>
 
 <script lang="ts">
-import { uploadState } from 'coghent-vue-3-component-library/lib'
+import { uploadState, Metadata } from 'coghent-vue-3-component-library'
 import { defineComponent, ref } from 'vue'
 import { useI18n } from 'vue-i18n'
-import { MetadataQuestion } from './UploadStepThree.vue'
 
 export default defineComponent({
   setup() {
     const { t } = useI18n()
-    const metadata = ref<Array<MetadataQuestion>>([])
+    const metadata = ref<Array<typeof Metadata>>([])
 
-    const setMetadata = () => {
-      for (let index = 1; index <= 4; index++) {
-        const answer = uploadState.metadata
-        metadata.value.push({
-          text: t(`myWorks.upload.stepThree.metadata.q${index}`),
-          answer: null,
-          key: null,
-        })
-      }
-    }
+    const init = () => {}
 
-    const init = () => {
-      setMetadata()
-    }
+    init()
 
     return {
       t,
+      uploadState,
     }
   },
 })
