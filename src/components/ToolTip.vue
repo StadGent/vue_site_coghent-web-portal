7f3cb32743a8817963cb097236c098643b67f6db
Berend <berend@inuits.eu>
2022-08-09 13:12:17 +0200

    Created tooltip to notify users

---
+++ src/components/ToolTip.vue
@@ -0,0 +1,53 @@
+<template>
+  <VDropdown v-model:shown="toolTipState.shown" :placement="placement" :triggers="[]" auto-hide>
+    <slot></slot>
+    <template #popper>
+      <div class="p-4">
+        <h1 class="text-2xl font-bold text-accent-purple">{{ title }}</h1>
+        <p>{{ description }}</p>
+      </div>
+    </template>
+  </VDropdown>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+type ToolTipState = {
+  shown: boolean
+}
+
+const toolTipState = ref<ToolTipState>({
+  shown: false,
+})
+
+export const useTooltip = () => {
+  const updateToolTipShownState = (input: boolean) => {
+    toolTipState.value.shown = input
+  }
+
+  return { updateToolTipShownState, toolTipState }
+}
+
+export default defineComponent({
+  props: {
+    title: {
+      type: String,
+      required: false,
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+    placement: {
+      type: String,
+      required: false,
+    },
+  },
+  setup(props) {
+    const { toolTipState } = useTooltip()
+
+    return { toolTipState }
+  },
+})
+</script>
