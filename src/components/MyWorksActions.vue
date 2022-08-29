41763da45ed04b55bd8b8a651a73e0c847ff0e80
Berend <berend@inuits.eu>
2022-08-29 11:49:11 +0200

    Refs #98756 Open Confirmation modal instead of deleting right away

--- src/components/MyWorksActions.vue
+++ src/components/MyWorksActions.vue
@@ -124,7 +124,7 @@ export default defineComponent({
 
     const openConfirm = () => {
       setConfirmationCallback(deleteRestoreAsset)
-      deleteRestoreAsset()
+      openConfirmationModal()
     }
 
     const init = () => {
