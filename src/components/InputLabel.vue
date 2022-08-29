06fa66093d660e0f1c9cccf38913a1caab2b59b6
Berend <berend@inuits.eu>
2022-08-29 09:54:22 +0200

    Refs #98650 Create inputlabel component with required star

---
+++ src/components/InputLabel.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex text-lg">
+    <label class="font-bold mb-2" :for="labelFor">{{ label }}</label
+    ><span v-if="inputIsRequired" class="px-2 text-accent-purple">*</span>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'inputLabel',
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    labelFor: {
+      type: String,
+      required: true,
+    },
+    inputIsRequired: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup() {},
+})
+</script>
