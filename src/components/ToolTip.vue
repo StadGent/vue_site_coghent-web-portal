612b97b6f81ca39d5acb09b547af83f41c119d1f
Berend <berend@inuits.eu>
2022-10-19 11:11:09 +0200

    More dynamic tooltip component

--- src/components/ToolTip.vue
+++ src/components/ToolTip.vue
@@ -2,19 +2,37 @@
   <VDropdown v-model:shown="toolTipState.shown" :placement="placement" :triggers="triggers" auto-hide>
     <slot></slot>
     <template #popper>
-      <div class="p-4">
-        <h1 class="text-2xl font-bold text-accent-purple">{{ title }}</h1>
-        <p>{{ description }}</p>
+      <div class="p-4 max-w-sm">
+        <h1 v-if="title" class="text-2xl font-bold text-accent-purple">{{ title }}</h1>
+        <p v-if="description">{{ description }}</p>
       </div>
     </template>
   </VDropdown>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType, ref } from 'vue'
+import { defineComponent, Prop, PropType, ref } from 'vue'
 
 type ToolTipState = {
   shown: boolean
+}
+
+enum Placement {
+  'auto',
+  'auto-start',
+  'auto-end',
+  'top',
+  'top-start',
+  'top-end',
+  'right',
+  'right-start',
+  'right-end',
+  'bottom',
+  'bottom-start',
+  'bottom-end',
+  'left',
+  'left-start',
+  'left-end',
 }
 
 const toolTipState = ref<ToolTipState>({
