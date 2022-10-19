244d9d98c7fd4cc0e974174f3689a5d764c09e77
Berend <berend@inuits.eu>
2022-10-19 17:12:35 +0200

    Refs #100770 Change card title to the same value as button title

--- src/components/onBoardingCarousel.vue
+++ src/components/onBoardingCarousel.vue
@@ -43,7 +43,7 @@ type CarouselState = {
 const carouselState = ref<CarouselState>({
   currentCarouselSlide: 1,
   carouselContent: [
-    { contentLocation: '/loginOnboarding.png', contentName: 'Login', contentDescription: 'main.onBoarding.login', descriptionLocation: 'top-20 right-0' },
+    { contentLocation: '/loginOnboarding.png', contentName: 'Aanmelden', contentDescription: 'main.onBoarding.login', descriptionLocation: 'top-20 right-0' },
     { contentLocation: '/loginOnboarding.png', contentName: 'Zoeken', contentDescription: 'main.onBoarding.search', descriptionLocation: 'top-28 right-0' },
     { contentLocation: '/storyboxOnboarding.png', contentName: 'Verhalen', contentDescription: 'main.onBoarding.storybox', descriptionLocation: 'top-20 right-0' },
     { contentLocation: '/storiesOnboarding.png', contentName: 'Ga aan de slag', contentDescription: 'main.onBoarding.stories', descriptionLocation: 'top-48 right-0' },
