cac24f01fa77fb81cf45941f1740b784086168d7
Berend <berend@inuits.eu>
2022-10-25 16:07:45 +0200

    Simplified header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -3,24 +3,7 @@
     <div class="flex gap-2 xl:gap-8">
       <div class="flex flex-col items-center">
         <a class="flex items-center sm:ml-0 ml-3 group cursor-pointer" @click="checkAction">
-          <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <rect width="48" height="48" fill="#FDC20B" />
-            <g filter="url(#filter0_i)">
-              <circle cx="24" cy="24" r="12" fill="white" />
-            </g>
-            <defs>
-              <filter id="filter0_i" x="12" y="12" width="26" height="26" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                <feFlood flood-opacity="0" result="BackgroundImageFix" />
-                <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
-                <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
-                <feOffset dx="2" dy="2" />
-                <feGaussianBlur stdDeviation="2" />
-                <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
-                <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.2 0" />
-                <feBlend mode="normal" in2="shape" result="effect1_innerShadow" />
-              </filter>
-            </defs>
-          </svg>
+          <img src="../assets/coghent.svg" href="logo" />
           <div class="sm:flex flex-col items-center mt-5 ml-2 hidden">
             <h2 class="md:text-lg text-center group-hover:underline" :class="{ [`font-bold`]: isHomeActive }">
               {{ t('header.discover') }}
