c9c78ca17dbb90a7a279f07b57540eab381f6dd3
Berend <berend@inuits.eu>
2022-10-28 15:41:16 +0200

    Add pager to testimony page

--- src/pages/TheTestimonyPage.vue
+++ src/pages/TheTestimonyPage.vue
@@ -7,6 +7,11 @@
   <section class="md:flex md:mt-4 gap-8">
     <profile-side-menu />
     <section class="w-full p-4 md:p-0">
+      <div v-if="pager.pageAmount.value > 1" class="w-full flex justify-end items-center">
+        <BaseIcon icon="arrowLeftLine" class="stroke-current p-2 cursor-pointer" @click="pager.goToPreviousPage(refetchWithProps)" />
+        <p>{{ `${pager.currentPage.value} of ${pager.pageAmount.value}` }}</p>
+        <BaseIcon icon="arrowRightLine" class="stroke-current p-2 cursor-pointer" @click="pager.goToNextPage(refetchWithProps)" />
+      </div>
       <profile-list-item v-for="testimonyListItem of testimonyList" :key="testimonyListItem.id" :profile-list-item-info="testimonyListItem" @deleteItem="confirmDelete" />
       <div v-if="loadingTestimonies && testimonyList.length === 0" class="h-fit p-8 flex flex-col w-full justify-center items-center overflow-hidden">
         <div class="flex justify-center items-center w-full p-4"><CircleLoader /></div>
@@ -22,23 +27,25 @@
 import { defineComponent, onMounted, ref, watch } from 'vue'
 import ProfileSideMenu from '../components/ProfileSideMenu.vue'
 import ProfileListItem, { ProfileListItemInfo, ProfileListItemType } from '../components/ProfileListItem.vue'
-import { GetTestimoniesOfUserDocument, Entity, CircleLoader, Relation, RelationType, DeleteEntityDocument } from 'coghent-vue-3-component-library'
+import { GetTestimoniesOfUserDocument, Entity, CircleLoader, Relation, RelationType, DeleteEntityDocument, BaseIcon } from 'coghent-vue-3-component-library'
 import { useMutation, useQuery } from '@vue/apollo-composable'
 import { parseDateAsLocaleString } from '@/helpers'
 import { useI18n } from 'vue-i18n'
 import { iiif } from '@/app'
 import ConfirmationModal, { useConfirmationModal } from '../components/ConfirmationModal.vue'
+import { Pager } from '../composables/pager'
 
 export default defineComponent({
   name: 'TheTestimonyPage',
-  components: { ProfileSideMenu, ProfileListItem, CircleLoader, ConfirmationModal },
+  components: { ProfileSideMenu, ProfileListItem, CircleLoader, ConfirmationModal, BaseIcon },
   setup() {
     const testimonyList = ref<typeof Entity[]>([])
-    const { result: testimonies, refetch: refetchTestimonies, loading: loadingTestimonies } = useQuery(GetTestimoniesOfUserDocument)
+    const { result: testimonies, refetch: refetchTestimonies, loading: loadingTestimonies } = useQuery(GetTestimoniesOfUserDocument, { limit: 0, skip: 0 })
     const { generateUrl, generateInfoUrl, noImageUrl } = iiif
     const { t } = useI18n()
     const { mutate: deleteItem } = useMutation(DeleteEntityDocument)
     const { setConfirmationCallback, setConfirmationEntityId, ConfirmationModalState, openConfirmationModal } = useConfirmationModal()
+    const pager = new Pager(6)
 
     const parseListItem = (items: typeof Entity[]): ProfileListItemInfo[] => {
       const listItems: ProfileListItemInfo[] = []
@@ -60,15 +67,21 @@ export default defineComponent({
       return listItems
     }
 
+    const refetchWithProps = () => {
+      testimonyList.value = []
+      refetchTestimonies({ limit: pager.limit.value, skip: pager.skip.value })
+    }
+
     onMounted(() => {
-      refetchTestimonies()
+      refetchWithProps()
     })
 
     watch(
       () => testimonies.value,
       () => {
         if (testimonies.value) {
-          testimonyList.value = parseListItem(testimonies.value.GetTestimoniesOfUser)
+          testimonyList.value = parseListItem(testimonies.value.GetTestimoniesOfUser.results)
+          pager.updateCount(testimonies.value.GetTestimoniesOfUser.count)
         }
       },
       { immediate: true }
@@ -88,7 +101,7 @@ export default defineComponent({
       }
     }
 
-    return { testimonyList, loadingTestimonies, t, deleteTestimony, confirmDelete }
+    return { testimonyList, loadingTestimonies, t, deleteTestimony, confirmDelete, pager, refetchWithProps }
   },
 })
 </script>
